e64c3a2c3e6ff5a0b7e47e8e400432b8722c540d
NatalioF22 <nataliogomes707@gmail.com>
2024-05-13 19:23:31 +0300

    first commit

--- git-push.sh
+++ git-push.sh
@@ -3,6 +3,6 @@ git add .
 echo "Enter a commit message: "
 read commitMsg
 git commit -m "$commitMsg"
-eacho "What branch should be pushed to: "
+echo "What branch should be pushed to: "
 rech branch
 git push -u origin "$branch"
